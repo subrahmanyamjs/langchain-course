e5745b58fb319d78ebe2381b33d9302bd21e769c
J Subrahmanyam <smanyam.j88@outlook.com>
2026-05-14 16:19:02 +0530

    update main

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-def main():
-    print("Hello from langchain-course!")
+from dotenv import load_dotenv
+import os
+load_dotenv()
 
+def main():
+    print("Hello, World!")
+    print(os.environ.get("OPENAI-API-KEY"))
 
 if __name__ == "__main__":
     main()
